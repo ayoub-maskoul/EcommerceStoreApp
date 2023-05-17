5fb276fb92148fde80a4e35930c7dfe7a5497179
ayoub-maskoul <maskoul.ayoub@ofppt-edu.ma>
2023-05-17 13:54:27 +0100

    Add Bottom Navigation

--- app/src/main/java/com/example/mystage/ShopingActivity.kt
+++ app/src/main/java/com/example/mystage/ShopingActivity.kt
@@ -2,10 +2,21 @@ package com.example.mystage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mystage.databinding.ActivityShopingBinding
 
 class ShopingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShopingBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shoping)
+        setContentView(binding.root)
+
+        val navController =findNavController(R.id.shoping_actiivity)
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/mystage/fragments/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.example.mystage.fragments
+
+import androidx.fragment.app.Fragment
+
+class ProfileFragment: Fragment() {
+}
