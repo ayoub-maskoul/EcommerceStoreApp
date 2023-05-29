f906da026987bbb176eae59838d271d79a0ad016
ayoub-maskoul <maskoul.ayoub@ofppt-edu.ma>
2023-05-29 19:20:52 +0100

    Add interface favorite

---
+++ app/src/main/java/com/example/mystage/fragments/FavoriteFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mystage.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.mystage.R
+import com.example.mystage.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment:Fragment(R.layout.fragment_favorite) {
+    private lateinit var binding: FragmentFavoriteBinding
+}
